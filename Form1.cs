7131b06ca702283e38392080c8ce9f1360466d00
Oleg <bababyby@kek.ru>
2024-11-30 00:42:22 +0500

    Бд открывается

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+
 
 namespace MDI_Oleg
 {
     public partial class Form1 : Form
     {
+        private SQLiteConnection Connection;
         public Form1()
         {
             InitializeComponent();
+            string path = "C:\\Users\\Олег\\Documents\\trbd\\MDI_Oleg\\oleg.db";
+            Connection = new SQLiteConnection($"Data Source={path};Version=3");
+            Connection.Open();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Connection.Close();
+        }
+
+        private void LoadStudents()
+        {
+            string query = "SELECT * FROM Client";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, Connection);
+            DataTable studentsTable = new DataTable();
+            adapter.Fill(studentsTable);
+            dataGridView1.DataSource = studentsTable;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadStudents();
         }
     }
 }
