f4bbf1f651c9e5d23af2a35e2f4bec10529c9733
Oleg <bababyby@kek.ru>
2024-12-05 15:05:09 +0500

    Отдельнопроцессное создание отчетов

--- ReportFree.cs
+++ ReportFree.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using Word = Microsoft.Office.Interop.Word;
@@ -16,7 +18,7 @@ namespace MDI_Oleg
             InitializeComponent();
         }
 
-        private void Confirm_Click(object sender, System.EventArgs e)
+        private void make_report(string path)
         {
             var command = Connection.CreateCommand();
             command.CommandText = $@"
@@ -32,7 +34,7 @@ namespace MDI_Oleg
                 );
             ";
             command.Parameters.AddWithValue("$Start", Start.Value);
-            command.Parameters.AddWithValue("$End",   End.Value);
+            command.Parameters.AddWithValue("$End", End.Value);
 
             var adapter = new SQLiteDataAdapter(command);
             var Table = new DataTable();
@@ -49,10 +51,24 @@ namespace MDI_Oleg
                 for (int i = 0; i < Table.Rows.Count; i++)
                     Sheet.Cells[i + 3, j + 1] = Table.Rows[i][j];
             }
-            Book.SaveAs("FreeCarsReport.xlsx");
+            Book.SaveAs(path);
             App.UserControl = true;
             App.Quit();
             MessageBox.Show("Отчет сформирован!");
+        }
+
+        private void Confirm_Click(object sender, System.EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            var path = saveFileDialog1.FileName;
+            if (File.Exists(path))
+                File.Delete(path);
+            new Thread(() => { 
+                make_report(saveFileDialog1.FileName); 
+            }).Start();
         }
 
         private void Cancel_Click(object sender, System.EventArgs e)
